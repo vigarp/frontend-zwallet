00f5b513b8ed54cca016058878ec7f148a0848e2
vigarp <vigar.8421@outlook.com>
2022-02-21 05:36:49 +0700

    add public/private route by token

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 // import internal modules
-import React, { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
 // import external modules
 import Footer from '../../components/module/Footer';
@@ -8,6 +8,14 @@ import Navbar from '../../components/module/Navbar';
 import Sidebar from '../../components/module/Sidebar';
 
 const Main = () => {
+  const auth = localStorage.getItem('token');
+  if (!auth) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/auth/login" />;
+  }
   return (
     <div className="min-vh-100 bg-light">
       <Navbar />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 const LandingPage = () => {
+  const auth = localStorage.getItem('token');
+  if (auth) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/main/home" />;
+  }
   return (
     <div>LandingPage</div>
   )
