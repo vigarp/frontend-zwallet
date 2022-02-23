2a9a1775be69538ed4b30acfa763b2d0661c4050
vigarp <vigar.8421@outlook.com>
2022-02-23 12:03:40 +0700

    add form validate in login page

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -7,23 +7,50 @@ import { Link } from 'react-router-dom';
 import { LoginUser } from '../../../redux/actions/login';
 import Input from '../../../components/base/Input';
 import Button from '../../../components/base/Button';
+import './login.css'
 
 const Login = () => {
-  const dispatch = useDispatch()
-  // const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const [formLogin, setFormLogin] = useState({
     email: '',
     password: ''
   })
+  const [formLoginError, setFormLoginError] = useState({});
   const handleChange = (e) => {
     setFormLogin({
       ...formLogin,
       [e.target.name]: e.target.value
     })
   }
-  const handleClick = () => {
-    dispatch((LoginUser(formLogin)))
+  
+  const validateLogin = (values) => {
+    const errors = {};
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Email address is invalid";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 6) {
+      errors.password = "Password must be more than 6 characters";
+    }
+    return errors;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const resultValidate = validateLogin(formLogin);
+    setFormLoginError(resultValidate);
+    handleClick(resultValidate);
+  }
+
+  const handleClick = (resultValidate) => {
+    if (Object.keys(resultValidate).length === 0) {
+      dispatch((LoginUser(formLogin)))
+    }
   }
   return (
     <div className="my-5 mx-5">
@@ -39,6 +66,7 @@ const Login = () => {
           placeholder="Enter your e-mail"
           onChange={handleChange}
           value={formLogin.email} />
+        <div className="position-absolute text-danger handle-error">{formLoginError.email}</div>
         <div className="position-absolute my-3 mx-1"><img src={require("../../../assets/img/icons/lock_loginpage.svg").default} alt="icon-lock-loginpage" /></div>
         <Input
           type="password"
@@ -46,8 +74,9 @@ const Login = () => {
           placeholder="Enter your password"
           onChange={handleChange}
           value={formLogin.password} />
+          <div className="position-absolute text-danger handle-error">{formLoginError.password}</div>
         <div className="text-end"><Link to={"/auth/forgot-password"} style={{ textDecoration: 'none' }}>Forgot Password?</Link></div>
-        <Button isLoading={undefined} onClick={handleClick} className="py-3 px-5 bg-secondary bg-opacity-50 text-white w-100 rounded-3 mt-5 fw-bold">Login</Button>
+        <Button isLoading={undefined} onClick={handleSubmit} className="py-3 px-5 bg-secondary bg-opacity-50 text-white w-100 rounded-3 mt-5 fw-bold">Login</Button>
         <div className="text-center mt-3">Don't have an account? Let's <Link to={"/auth/signup"} style={{ textDecoration: 'none' }}> Sign Up</Link></div>
       </div>
     </div>
