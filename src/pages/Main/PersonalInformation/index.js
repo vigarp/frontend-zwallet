a47ac588e74bd9833c14c901049eeca633a641a2
vigarp <vigar.8421@outlook.com>
2022-02-23 07:23:03 +0700

    add personalInformation page

--- src/pages/Main/PersonalInformation/index.js
+++ src/pages/Main/PersonalInformation/index.js
@@ -1,49 +1,45 @@
-import React, {Fragment} from 'react'
+// import internal modules
+import React, {Fragment, useEffect, useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// import external modules
+import { GetUserDetail } from '../../../redux/actions/user';
 
 const PersonalInformation = () => {
+  const dispatch = useDispatch();
+  const userDetailData = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch((GetUserDetail()))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Fragment>
       <article className="bg-white rounded g-0 p-4">
-        <div className="w-100 text-center mx-3">
-          <div className="text-center g-0 ps-3 my-3"><img src={require("../../../assets/img/icons/checklist_transfer_success.svg").default} alt="" /></div>
-          <div className="text-center g-0 ps-3 my-3 fw-bold">Transfer Success</div>
+        <div className="w-100 mx-3">
+          <div className="g-0 ps-3 my-3 fw-bold">Personal Information</div>
+          <div className="g-0 ps-3 my-3 text-muted">We got your personal information from the sign<br />up proccess. If you want to make changes<br />on your information, contact our support.</div>
         </div>
         <div className="rounded p-3 row g-0 me-3 my-4">
           <div className="col lh-lg ps-3">
-            <div className="text-muted">Amount</div>
-            <div className="fw-bold">Rp. 12</div>
-          </div>
-        </div>
-        <div className="rounded p-3 row g-0 me-3 my-4">
-          <div className="col lh-lg ps-3">
-            <div className="text-muted">Balance Left</div>
-            <div className="fw-bold">Rp. 10</div>
+            <div className="text-muted">Name</div>
+            <div className="fw-bold">{userDetailData.data?.username}</div>
           </div>
         </div>
         <div className="rounded p-3 row g-0 me-3 my-4">
           <div className="col lh-lg ps-3">
-            <div className="text-muted">Date &#38; Time</div>
-            <div className="fw-bold">selasa</div>
+            <div className="text-muted">Verified E-mail</div>
+            <div className="fw-bold">{userDetailData.data?.email}</div>
           </div>
         </div>
-        <div className="rounded p-3 row g-0 me-3 my-4">
-          <div className="col lh-lg ps-3">
-            <div className="text-muted">Notes</div>
-            <div className="fw-bold">oke</div>
+        <div className="rounded p-3 d-flex g-0 me-3 my-4">
+          <div className="lh-lg ps-3 flex-grow-1">
+            <div className="text-muted">Phone Number</div>
+            <div className="fw-bold">{userDetailData.data?.phone}</div>
           </div>
-        </div>
-        <div className="fw-bold g-0 ps-3">Transfer to</div>
-        <div className="rounded py-3 bg-light row my-3">
-          <div className="col flex-grow-0 px-3"><img src={'picture.jpg'} width={60} height={60} alt="" /></div>
-          <div className="col my-3">
-            <div className="fw-bold">atok</div>
-            <div className="text-muted">mail.id</div>
-          </div>
-        </div>
-        <div className="d-flex justify-content-end">
-          <div className="bg-light rounded px-3 py-3 m-1 align-self-center"><img src={require("../../../assets/img/icons/button_share_transfer_success.svg").default} alt="" /></div>
-          <button className="bg-light rounded px-3 py-3 mx-3 align-self-center"><img src={require("../../../assets/img/icons/button_download_transfer_success.svg").default} alt="" />Download</button>
-          <button onClick={null} className="bg-primary bg-gradient w-25 rounded p-3 text-center">Back to Home</button>
+          <div className="my-3 text-primary text-decoration-none"><Link to={"/main/manage-phone-number"} style={{textDecoration: 'none'}}>Manage</Link></div>
         </div>
       </article>
     </Fragment>
