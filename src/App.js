1cad72e2f128a8e5484d48614d6f060f09edf14e
vigarp <vigar.8421@outlook.com>
2022-02-26 11:35:05 +0700

    add socket.on marks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import internal modules
 import React, {useEffect} from "react";
-import {io} from "socket.io-client";
+import socket from "./helpers/socket"
 
 // import external modules
 import Router from './config/Router'
@@ -8,8 +8,9 @@ import Router from './config/Router'
 // create function for App.js
 function App() {
   useEffect(() => {
-    const socket = io("http://localhost:4001")
-    console.log(socket)
+    socket.on('message for new user', (message) => {
+      alert(message)
+    })
   }, [])
   return (
     <Router />
