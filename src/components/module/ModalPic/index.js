4aac7f3112455e033822d00587d9de4e01525734
vigarp <vigar.8421@outlook.com>
2022-03-25 15:50:06 +0700

    modalpic using link

--- src/components/module/ModalPic/index.js
+++ src/components/module/ModalPic/index.js
@@ -11,6 +11,7 @@ const ModalPic = ({ openModal }) => {
   const dispatch = useDispatch();
 
   const [loading, setLoading] = useState(false);
+  const [formPictureError, setFormPictureError] = useState(false);
 
   const [formDataPicTemp, setFormDataPicTemp] = useState({
     picture: ""
@@ -18,11 +19,39 @@ const ModalPic = ({ openModal }) => {
   const formDataPic = new FormData()
   formDataPic.append("picture", formDataPicTemp.picture[0])
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setLoading(true)
-    dispatch((editPic(formDataPic, setLoading, openModal)))
+  const [formPicture, setFormPicture] = useState({
+    picture: ''
+  })
+
+  const handleChange = (e) => {
+    setFormPicture({
+      ...formPicture,
+      [e.target.name]: e.target.value
+    })
   }
+
+  const validatePicture = (values) => {
+    const errors = {};
+    if (!values.picture) {
+      errors.picture = "Picture link is required";
+    }
+    return errors;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const resultValidate = validatePicture(formPicture);
+    setFormPictureError(resultValidate);
+    handleClick(resultValidate)
+  }
+  const handleClick = (resultValidate) => {
+    if (Object.keys(resultValidate).length === 0) {
+      setFormPictureError(false)
+      setLoading(true)
+      dispatch((editPic(formPicture, setLoading, openModal)))
+    }
+  }
+  
   return (
     <div className="modal-wrapper">
       <div className="modal-inner">
@@ -34,7 +63,15 @@ const ModalPic = ({ openModal }) => {
         <div>Select File and upload the picture</div>
         <div className="my-3">Change Profile Picture</div>
         <form encType="multipart/form-data" onSubmit={(e) => handleSubmit(e)} className="d-flex flex-column">
-          <input type="file" onChange={(e) => setFormDataPicTemp({ ...formDataPicTemp, picture: e.currentTarget.files })} /><br />
+          <input disabled type="file" onChange={(e) => setFormDataPicTemp({ ...formDataPicTemp, picture: e.currentTarget.files })} /><br />
+          <input 
+          type="text"
+          name="picture"
+          placeholder="Please input an url link here"
+          onChange={handleChange}
+          value={formPicture.picture}
+          className="my-3" />
+          <div className="text-error">{formPictureError.picture}</div>
           <hr />
           <Button isLoading={loading} className="my-3 bg-primary bg-gradient px-3" type="submit">Upload</Button>
         </form>
