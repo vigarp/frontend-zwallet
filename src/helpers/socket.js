d7d0bababe081aca7f5569bad359c3369beef08f
vigarp <vigar.8421@outlook.com>
2022-03-28 10:16:59 +0700

    change socketio env

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client';
 
-const URL = "http://localhost:4001";
+const URL = `${process.env.REACT_APP_URL_WS}`;
 const socket = io(URL);
 
 socket.onAny((event, ...args) => {
