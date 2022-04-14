090547d39ea2e3aae50d8255b2b72320d9959b03
vigarp <vigar.8421@outlook.com>
2022-04-15 06:48:08 +0700

    add sidebar active location

--- src/components/module/Sidebar/index.js
+++ src/components/module/Sidebar/index.js
@@ -1,11 +1,12 @@
 // import internal modules
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 // import external modules
 import './sidebar.css';
 
 const Sidebar = () => {
+    const location = useLocation();
     const navigate = useNavigate();
     const handleLogout = () => {
         localStorage.clear()
@@ -15,20 +16,60 @@ const Sidebar = () => {
         <aside className="sidebar w-25 bg-white d-flex flex-column justify-content-between p-3 me-3">
             <div className="sidebar-upper">
                 <div className="row p-3">
-                    <div className="col flex-grow-0 py-1"><img src={require("../../../assets/img/icons/grid_homepage_plain.svg").default} alt="icon-grid-homepage-active" /></div>
-                    <div className="col text-decoration-none py-1 dashboard-pointer"><Link to={"/main"} style={{ textDecoration: 'none', color: 'black' }}>Dashboard</Link></div>
+                    {location.pathname === "/main/home" ? (
+                        <>
+                            <div className="col flex-grow-0 marker-icon"><img src={require("../../../assets/img/icons/marker_homepage copy.svg").default} alt="marker-icon" /></div>
+                            <div className="col flex-grow-0 py-1"><img src={require("../../../assets/img/icons/grid_homepage_active.svg").default} alt="icon-grid-homepage-active" /></div>
+                            <div className="col text-decoration-none py-1 dashboard-pointer"><Link to={"/main"} style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}>Dashboard</Link></div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="col flex-grow-0 py-1"><img src={require("../../../assets/img/icons/grid_homepage_plain.svg").default} alt="icon-grid-homepage-active" /></div>
+                            <div className="col text-decoration-none py-1 dashboard-pointer"><Link to={"/main"} style={{ textDecoration: 'none', color: 'black' }}>Dashboard</Link></div>
+                        </>
+                    )}
                 </div>
                 <div className="row p-3">
-                    <div className="col flex-grow-0 "><img src={require("../../../assets/img/icons/arrowup_homepage.svg").default} alt="icon-arrowup-homepage" /></div>
-                    <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/transfer"} style={{ textDecoration: 'none', color: 'black' }}>Transfer</Link></div>
+                    {location.pathname === "/main/transfer" ? (
+                        <>
+                            <div className="col flex-grow-0 marker-icon"><img src={require("../../../assets/img/icons/marker_homepage copy.svg").default} alt="marker-icon" /></div>
+                            <div className="col flex-grow-0 "><img src={require("../../../assets/img/icons/arrowup_homepage_active.svg").default} alt="icon-arrowup-homepage" /></div>
+                            <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/transfer"} style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}>Transfer</Link></div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="col flex-grow-0 "><img src={require("../../../assets/img/icons/arrowup_homepage.svg").default} alt="icon-arrowup-homepage" /></div>
+                            <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/transfer"} style={{ textDecoration: 'none', color: 'black' }}>Transfer</Link></div>
+                        </>
+                    )}
                 </div>
                 <div className="row p-3">
-                    <div className="col flex-grow-0"><img src={require("../../../assets/img/icons/plus_homepage.svg").default} alt="icon-plus-homepage" /></div>
-                    <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/topup"} style={{ textDecoration: 'none', color: 'black' }}>Top Up</Link></div>
+                    {location.pathname === "/main/topup" ? (
+                        <>
+                            <div className="col flex-grow-0 marker-icon"><img src={require("../../../assets/img/icons/marker_homepage copy.svg").default} alt="marker-icon" /></div>
+                            <div className="col flex-grow-0"><img src={require("../../../assets/img/icons/plus_homepage_active.svg").default} alt="icon-plus-homepage" /></div>
+                            <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/topup"} style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}>Top Up</Link></div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="col flex-grow-0"><img src={require("../../../assets/img/icons/plus_homepage.svg").default} alt="icon-plus-homepage" /></div>
+                            <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/topup"} style={{ textDecoration: 'none', color: 'black' }}>Top Up</Link></div>
+                        </>
+                    )}
                 </div>
                 <div className="row p-3">
-                    <div className="col flex-grow-0"><img src={require("../../../assets/img/icons/user_homepage.svg").default} alt="icon-user-homepage" /></div>
-                    <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/profile"} style={{ textDecoration: 'none', color: 'black' }}>Profile</Link></div>
+                    {location.pathname === "/main/profile" ? (
+                        <>
+                            <div className="col flex-grow-0 marker-icon"><img src={require("../../../assets/img/icons/marker_homepage copy.svg").default} alt="marker-icon" /></div>
+                            <div className="col flex-grow-0"><img src={require("../../../assets/img/icons/user_homepage_active.svg").default} alt="icon-user-homepage" /></div>
+                            <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/profile"} style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}>Profile</Link></div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="col flex-grow-0"><img src={require("../../../assets/img/icons/user_homepage.svg").default} alt="icon-user-homepage" /></div>
+                            <div className="col text-decoration-none dashboard-pointer"><Link to={"/main/profile"} style={{ textDecoration: 'none', color: 'black' }}>Profile</Link></div>
+                        </>
+                    )}
                 </div>
             </div>
             <div className="sidebar-lower">
